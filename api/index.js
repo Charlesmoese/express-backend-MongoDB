0c5a67c5d1fd07d5202a84169e00600f8d848278
Charles Moese <charlesmoesefilho@gmail.com>
2025-05-03 14:47:30 +0000

    add rotas, login e autenticacao

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from "dotenv";
 import db from "./database/configdb.js";
 import User from "./models/User.js";
+import userRoute from "../routes/user.route.js";
+import tokenRoute from './routes/token.route.js';
 
 
 dotenv.config();
@@ -11,6 +13,9 @@ db.connect();
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRoute);
+app.use("/", tokenRoute);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
---
+++ api/routes/user.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import userController from '../controller/user.controller.js';
+
+const router = express.Router();
+
+router.post('/register', userController.register);
+
+router.post('/login', userController.login);
+
+router.get('/users', userController.getAllUsers);
+
+
+export default router;
